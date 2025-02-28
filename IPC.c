c620c20f6f50a8c0407cb58239e5f93b2d063360
DWC2003 <dwc110499@gmail.com>
2025-02-28 14:59:23 -0500

    Added comments

--- IPC.c
+++ IPC.c
@@ -1,47 +1,57 @@
 #include <sys/types.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 4096 // Constant buffer size
 
 int main(int argc, char *argv[]){
-    pid_t pid;
-    char buffer[BUFFER_SIZE];
-    int readb;
-    int p[2];
+    pid_t pid; // Process ID
+    char buffer[BUFFER_SIZE]; // Buffer for pipe
+    int readb; // Bytes read
+    int p[2]; // Pipe array
+
+    // Variables for final report
     int failedDeliveries = 0;
     int successfulDeliveries = 0;
 
-    if (argv[1] == ""){
-        printf("Must provide argument.");
+
+    if (argv[1] == NULL){ // Checks for argument
+        printf("Must provide argument.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (atoi(argv[1]) == 0){ // 0 Delivery trucks
+        perror("Must be an integer and more than 0.\n");
+        exit(EXIT_FAILURE);
     }
 
-    if (pipe(p) < 0){
+    if (pipe(p) < 0){ // Creates pipe
         perror("pipe");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    pid = fork();
+    pid = fork(); // Forks process
 
-    if (pid < 0){
-        printf(stderr, "Fork Failed");
-        return 1;
+    if (pid < 0){ // Failed to fork process
+        perror("Fork Failed");
+        exit(EXIT_FAILURE);
     }
-    else if (pid == 0){
-        close(p[0]);
-        dup2(p[1], STDOUT_FILENO);
+    else if (pid == 0){ // Child process
+        close(p[0]); // Close read end
+        dup2(p[1], STDOUT_FILENO); // Redirect output into pipe
         close(p[1]);
 
+        // Replace child process with multi-threading application
         execlp("./MultiThreading", "MultiThreading", argv[1], (char *)NULL);
     }
-    else{
-        close(p[1]);
+    else{ // Parent process
+        close(p[1]); // Close write end
         printf("Beginning child process, this may take a minute.\n");
         printf("%d trucks with 10 deliveries each.\nPlease wait...\n", atoi(argv[1]));
 
-        while ((readb = read(p[0], buffer, BUFFER_SIZE - 1)) > 0){
+        while ((readb = read(p[0], buffer, BUFFER_SIZE - 1)) > 0){ // Reads string from buffer continuously
             buffer[readb] = '\0';
             for (int i = 0; i < readb; i++){
                 if (buffer[i] == '\n'){
@@ -54,6 +64,8 @@ int main(int argc, char *argv[]){
                 }
             }
         }
+
+        // Generates report
         printf("Failed Deliveries: %d\n", failedDeliveries);
         printf("Successful Deliveries: %d/%d\n", successfulDeliveries, atoi(argv[1])*10);
         if (failedDeliveries > successfulDeliveries){
@@ -69,6 +81,7 @@ int main(int argc, char *argv[]){
 
         close(p[0]);
 
+        // Waits on child process
         wait(NULL);
         printf("Child process complete.\n");
     }
--- MultiThreading.c
+++ MultiThreading.c
@@ -3,34 +3,39 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// Global variables/Shared resources
 int netProfit = 0;
 int storeStock = 250;
+// Trucks is the same as the number of threads - 1
 int trucks = 0;
+// Mutex locks
 pthread_mutex_t lock;
 pthread_mutex_t lock2;
 
+// Structure to be passed to thread function
 struct Input{
-    int tasks[100];
-    int id;
+    int tasks[100]; // List of deliveries/shipments. Max of 100.
+    int id; // Designates what type of truck
 };
 
+// Thread function
 void* runner(void* param){
     int i;
-    struct Input *info = (struct Input*)param;
+    struct Input *info = (struct Input*)param; // Structure for delivery/shipment information
 
-    int delnum = 0;
-    static int shipnum = 0;
-    bool flag = true;
+    int delnum = 0; // Number of deliveries done
+    static int shipnum = 0; // Number of shipments done. Can be accessed by all
+    bool flag = true; // Flag for outputting failed shipment so it doesn't output constantly
 
     while (delnum < 10 || shipnum < 100){
-        pthread_mutex_lock(&lock);
+        pthread_mutex_lock(&lock); // Locks code with first lock
 
-        if (info->id < trucks){
+        if (info->id < trucks){ // Delivery truck segment
             if (delnum >= 10){
                 pthread_mutex_unlock(&lock);
                 break;
             }
-            if ((storeStock - info->tasks[delnum]) < 0){
+            if ((storeStock - info->tasks[delnum]) < 0){ // Not enough stock for delivery
                 if (shipnum >= 35){
                     printf("\nNot enough stock for truck %d(%d deliveries remaining). \n", (info->id)+1, 10-(delnum));
                     pthread_mutex_unlock(&lock);
@@ -45,11 +50,12 @@ void* runner(void* param){
                     continue;
                 }
             }
-            else{
+            else{ // Successful delivery
                 printf("\n Delivery number %d has left in truck %d with %d products. S\n", (delnum+1), ((info->id) + 1), info->tasks[delnum]);
                 storeStock -= info->tasks[delnum];
                 printf("Current stock: %d\n", storeStock);
 
+                // Second mutex lock to access the other shared resource
                 pthread_mutex_lock(&lock2);
                 netProfit += 2*info->tasks[delnum];
                 printf("Current net profit: %d\n", netProfit);
@@ -60,22 +66,26 @@ void* runner(void* param){
 
                 
             }
-        } 
+        } // Shipment truck segment
         else{
             if (shipnum >= 35){
                 pthread_mutex_unlock(&lock);
                 break;
             }
+
             printf("\n Shipment number %d has arrived with %d products.\n", (shipnum+1), (info->tasks[shipnum]));
             storeStock += info->tasks[shipnum];
             printf("Current stock: %d\n", storeStock);
 
+            // Second mutex lock to access the other shared resource
             pthread_mutex_lock(&lock2);
             netProfit -= info->tasks[shipnum];
             printf("Current net profit: %d\n", netProfit);
             pthread_mutex_unlock(&lock2);
             shipnum++;
         }
+
+        // Releases lock and pauses for 1 millisecond
         pthread_mutex_unlock(&lock);
         sleep(1);
 
@@ -85,7 +95,17 @@ void* runner(void* param){
 
 int main(int argc, char *argv[]){
     int i;
-    trucks = atoi(argv[1]);
+    if (argv[1] == NULL){ // Checks for argument
+        perror("No argument.");
+        exit(EXIT_FAILURE);
+    }
+    trucks = atoi(argv[1]); // Gets number of trucks from argument 1
+    if (trucks == 0){ // 0 Delivery trucks
+        perror("Must be an integer or more than 0.");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prints number of delivery trucks and allocates memory for all of their deliveries
     printf("Delivery trucks: %d\n",trucks);
     struct Input *deliveries[trucks];
     for (i = 0; i < trucks; i++){
@@ -97,31 +117,36 @@ int main(int argc, char *argv[]){
         }
     }
       
+    // Allocates memory for shipments
     struct Input *shipment = (struct Input*) malloc(sizeof(struct Input));
     shipment->id = trucks;
     for (i = 0; i < 35; i++){
         shipment->tasks[i] = 100+i;
     }
 
+    // Day begins
     printf("\nStart of day stock: %d\n", storeStock);
 
+    // pthread initializations
     pthread_t tid[trucks];
     pthread_attr_t attr;
     pthread_mutex_init(&lock, NULL);
     pthread_mutex_init(&lock2, NULL);
     pthread_attr_init(&attr);
 
+    // Creates all threads for deliveries and then shipment thread
     for (i = 0; i < trucks; i++){
         pthread_create(&tid[i], &attr, runner, deliveries[i]);
 
     }
     pthread_create(&tid[trucks], &attr, runner, shipment);
     
-
+    // Joins threads
     for (i = 0; i < trucks+1; i++){
         pthread_join(tid[i], NULL);
     }
 
+    // Destroys lock and outputs end of day stats
     pthread_mutex_destroy(&lock);
 
     printf("\nEnd of day stock: %d\nEnd of day net profit: %d\n", storeStock, netProfit);
